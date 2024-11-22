ff6213b50b93dd59170dc4a497c5b999271da923
Ganesh Pavan Kumar <ganesh.pavan.kumar@walmart.com>
2024-11-23 01:19:15 +0530

    episode9

---
+++ episode9/src/components/Header.js
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
+
+const Header = () => {
+  const [btnName, setbtnName] = useState("Logout");
+  const onlineStatus = useOnlineStatus();
+
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>online status: {onlineStatus ? "online" : "offline"}</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li>
+            <Link to="/grocery">Grocery</Link>
+          </li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnName === "Login" ? setbtnName("Logout") : setbtnName("Login");
+            }}
+          >
+            {btnName}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
